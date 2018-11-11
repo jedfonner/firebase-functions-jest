bc0555fe8b860141ba15bb68251fc1c8b0ac1993
Jed Fonner <github@jedfonner.com>
2018-11-11 08:07:09 -0500

    Prettier-ize uppercase functions

--- uppercase/functions/index.js
+++ uppercase/functions/index.js
@@ -13,13 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict';
+"use strict";
 
 // [START all]
 // [START import]
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 // [END import]
 
@@ -28,14 +28,18 @@ admin.initializeApp(functions.config().firebase);
 // Realtime Database under the path /messages/:pushId/original
 // [START addMessageTrigger]
 exports.addMessage = functions.https.onRequest((req, res) => {
-// [END addMessageTrigger]
+  // [END addMessageTrigger]
   // Grab the text parameter.
   const original = req.query.text;
   // Push it into the Realtime Database then send a response
-  admin.database().ref('/messages').push({original: original}).then(snapshot => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref);
-  });
+  admin
+    .database()
+    .ref("/messages")
+    .push({ original: original })
+    .then(snapshot => {
+      // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+      res.redirect(303, snapshot.ref);
+    });
 });
 // [END addMessage]
 
@@ -43,19 +47,20 @@ exports.addMessage = functions.https.onRequest((req, res) => {
 // Listens for new messages added to /messages/:pushId/original and creates an
 // uppercase version of the message to /messages/:pushId/uppercase
 // [START makeUppercaseTrigger]
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-    .onWrite(event => {
-// [END makeUppercaseTrigger]
-      // [START makeUppercaseBody]
-      // Grab the current value of what was written to the Realtime Database.
-      const original = event.data.val();
-      console.log('Uppercasing', event.params.pushId, original);
-      const uppercase = original.toUpperCase();
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      return event.data.ref.parent.child('uppercase').set(uppercase);
-      // [END makeUppercaseBody]
-    });
+exports.makeUppercase = functions.database
+  .ref("/messages/{pushId}/original")
+  .onWrite(event => {
+    // [END makeUppercaseTrigger]
+    // [START makeUppercaseBody]
+    // Grab the current value of what was written to the Realtime Database.
+    const original = event.data.val();
+    console.log("Uppercasing", event.params.pushId, original);
+    const uppercase = original.toUpperCase();
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to the Firebase Realtime Database.
+    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+    return event.data.ref.parent.child("uppercase").set(uppercase);
+    // [END makeUppercaseBody]
+  });
 // [END makeUppercase]
 // [END all]
