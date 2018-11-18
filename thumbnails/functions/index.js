192524e36559a84a7a949f78df7a120406fa7a40
Jed Fonner <github@jedfonner.com>
2018-11-18 08:28:41 -0500

    Revert thumbnail function to gcs version

--- thumbnails/functions/index.js
+++ thumbnails/functions/index.js
@@ -17,6 +17,7 @@
 
 // [START import]
 const functions = require("firebase-functions");
+const gcs = require("@google-cloud/storage")();
 const spawn = require("child-process-promise").spawn;
 const path = require("path");
 const os = require("os");
@@ -29,60 +30,66 @@ const fs = require("fs");
  * ImageMagick.
  */
 // [START generateThumbnailTrigger]
-exports.generateThumbnail = functions.storage
-  .object()
-  .onFinalize(async object => {
-    // [END generateThumbnailTrigger]
-    // [START eventAttributes]
-    const fileBucket = object.bucket; // The Storage bucket that contains the file.
-    const filePath = object.name; // File path in the bucket.
-    const contentType = object.contentType; // File content type.
-    const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
-    // [END eventAttributes]
+exports.generateThumbnail = functions.storage.object().onFinalize(object => {
+  // [END generateThumbnailTrigger]
+  // [START eventAttributes]
+  const fileBucket = object.bucket; // The Storage bucket that contains the file.
+  const filePath = object.name; // File path in the bucket.
+  const contentType = object.contentType; // File content type.
+  const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
+  // [END eventAttributes]
 
-    // [START stopConditions]
-    // Exit if this is triggered on a file that is not an image.
-    if (!contentType.startsWith("image/")) {
-      console.log("This is not an image.");
-      return null;
-    }
+  // [START stopConditions]
+  // Exit if this is triggered on a file that is not an image.
+  if (!contentType.startsWith("image/")) {
+    console.log("This is not an image.");
+    return null;
+  }
 
-    // Get the file name.
-    const fileName = path.basename(filePath);
-    // Exit if the image is already a thumbnail.
-    if (fileName.startsWith("thumb_")) {
-      console.log("Already a Thumbnail.");
-      return null;
-    }
-    // [END stopConditions]
+  // Get the file name.
+  const fileName = path.basename(filePath);
+  // Exit if the image is already a thumbnail.
+  if (fileName.startsWith("thumb_")) {
+    console.log("Already a Thumbnail.");
+    return null;
+  }
+  // [END stopConditions]
 
-    // [START thumbnailGeneration]
-    // Download file from bucket.
-    const bucket = admin.storage().bucket(fileBucket);
-    const tempFilePath = path.join(os.tmpdir(), fileName);
-    const metadata = {
-      contentType: contentType
-    };
-    await bucket.file(filePath).download({ destination: tempFilePath });
-    console.log("Image downloaded locally to", tempFilePath);
-    // Generate a thumbnail using ImageMagick.
-    await spawn("convert", [
-      tempFilePath,
-      "-thumbnail",
-      "200x200>",
-      tempFilePath
-    ]);
-    console.log("Thumbnail created at", tempFilePath);
-    // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
-    const thumbFileName = `thumb_${fileName}`;
-    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
-    // Uploading the thumbnail.
-    await bucket.upload(tempFilePath, {
-      destination: thumbFilePath,
-      metadata: metadata
-    });
-    // Once the thumbnail has been uploaded delete the local file to free up disk space.
-    fs.unlinkSync(tempFilePath);
-    // [END thumbnailGeneration]
-  });
+  // [START thumbnailGeneration]
+  // Download file from bucket.
+  const bucket = gcs.bucket(fileBucket);
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+  const metadata = {
+    contentType: contentType
+  };
+  return bucket
+    .file(filePath)
+    .download({
+      destination: tempFilePath
+    })
+    .then(() => {
+      console.log("Image downloaded locally to", tempFilePath);
+      // Generate a thumbnail using ImageMagick.
+      return spawn("convert", [
+        tempFilePath,
+        "-thumbnail",
+        "200x200>",
+        tempFilePath
+      ]);
+    })
+    .then(() => {
+      console.log("Thumbnail created at", tempFilePath);
+      // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
+      const thumbFileName = `thumb_${fileName}`;
+      const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+      // Uploading the thumbnail.
+      return bucket.upload(tempFilePath, {
+        destination: thumbFilePath,
+        metadata: metadata
+      });
+      // Once the thumbnail has been uploaded delete the local file to free up disk space.
+    })
+    .then(() => fs.unlinkSync(tempFilePath));
+  // [END thumbnailGeneration]
+});
 // [END generateThumbnail]
